884568f23f9babadeb9b64a87819fbc52b30ddca
tennguoi <nguyenduccongminh@gmail.com>
2026-07-21 09:52:34 +0700

    f

--- src/main/resources/db/manual/20260622_service_type_cleanup.sql
+++
@@ -1,5 +0,0 @@
--- Run once before deploying the ServiceType enum reduced to ACCOUNT_STOCK and MANUAL.
--- Legacy types already followed the manual-order branch, so they are normalized to MANUAL.
-UPDATE services
-SET type = 'MANUAL'
-WHERE type NOT IN ('ACCOUNT_STOCK', 'MANUAL');
